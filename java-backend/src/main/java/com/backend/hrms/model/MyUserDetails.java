2ba4df2536dd46ffd8749ba69ed3273ffbb8cc85
xack20 <zakariahossain20@gmail.com>
2021-12-08 20:32:41 +0600

    back-end

--- java-backend/src/main/java/com/backend/hrms/model/MyUserDetails.java
+++ java-backend/src/main/java/com/backend/hrms/model/MyUserDetails.java
@@ -11,6 +11,23 @@ import java.util.List;
 public class MyUserDetails implements UserDetails {
     private Long id;
     private String fullname;
+
+    public Long getId() {
+        return this.id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getFullname() {
+        return this.fullname;
+    }
+
+    public void setFullname(String fullname) {
+        this.fullname = fullname;
+    }
+
     private String username;
     private String password;
     private Collection<? extends GrantedAuthority> authorities;
