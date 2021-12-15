32284ffa680a2c8d5b58c8d0ec1c7d9e6d9a42de
xack20 <zakariahossain20@gmail.com>
2021-12-16 00:45:43 +0600

    back-end

--- client/src/Components/Title/Title.jsx
+++ client/src/Components/Title/Title.jsx
@@ -3,7 +3,7 @@ import './Title.css';
 
 class Title extends Component {
   render() {
-    return <h5 className="title">Semicolon IT Solutions HRMS</h5>;
+    return <h5 className="title">Human Resource Management System</h5>;
   }
 }
 
