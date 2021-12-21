df7e3222bb5cdb27b9427ad1bbb86c93e1cf143d
Mohammad Farhan Sadik <tusharsad27@gmail.com>
2021-12-21 17:46:48 +0600

    Employee dashboard change

--- client/src/Components/DividerVertical/DividerVertical.jsx
+++ client/src/Components/DividerVertical/DividerVertical.jsx
@@ -9,18 +9,18 @@ import { Row, Col } from "antd";
 const DividerVertical = () => {
   return (
     <div>
-      <Card style={{ backgroundColor: "#0050b3", color: "white", borderRadius: "25px" }}>
+      <Card style={{ backgroundColor: "#fff7e6", color: "black", borderRadius: "25px", borderColor:"#096dd9" }}>
         <Row >
           <Col span={6}>
             <p>14</p>
             <p>Assigned Token</p>
           </Col>
-          <Divider type="vertical" style={{ 'background-color': 'white', height: "80px" }} />
+          <Divider type="vertical" style={{ 'background-color': 'black', height: "80px" }} />
           <Col span={6}>
             <p>14</p>
             <p>Pending Token</p>
           </Col>
-          <Divider type="vertical" style={{ 'background-color': 'white', height: "80px" }} />
+          <Divider type="vertical" style={{ 'background-color': 'black', height: "80px" }} />
           <Col span={6}>
             <p>14</p>
             <p>Total Token</p>
--- client/src/Components/DividerVertical/DividerVerticalV2.jsx
+++ client/src/Components/DividerVertical/DividerVerticalV2.jsx
@@ -12,7 +12,7 @@ const DividerVerticalV2 = ({props}) => {
   return (
     <div>
       {console.log(props.type,props.data)}
-      <Card style={{ backgroundColor: "#006d75", color: "white", borderRadius: "25px" }}>
+      <Card style={{ backgroundColor: "#e6fffb", color: "black", borderRadius: "25px", borderColor:"#096dd9" }}>
         <Row >
           <Col span={12}>
             <div>
@@ -20,7 +20,7 @@ const DividerVerticalV2 = ({props}) => {
               <p>Approved</p>
             </div>
           </Col>
-          <Divider type="vertical" style={{ 'background-color': 'white', height: "65px" }} />
+          <Divider type="vertical" style={{ 'background-color': 'black', height: "65px" }} />
           <Col span={6}>
             <div>
               <p>14</p>
@@ -32,7 +32,7 @@ const DividerVerticalV2 = ({props}) => {
 
         <div class="row">
           <div class="col text-center">
-            <Button type="primary" style={{ background: "#ad6800" }} onClick={onApply}>Apply</Button>
+            <Button type="primary" style={{ background: "#531dab", borderRadius:"10px" }} onClick={onApply}>Apply</Button>
           </div>
         </div>
 
